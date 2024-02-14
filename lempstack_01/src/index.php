42c7d9c20b048307a0fcf5b979722e27001187be
ilknurm <ilknur.manav2@gmail.com>
2024-02-14 14:12:40 +0100

    fix syntac error

--- lempstack_01/src/index.php
+++ lempstack_01/src/index.php
@@ -5,6 +5,6 @@
     </head>
     <body>
         <?php echo '<p>Hello CHECK24</p>'; ?>
-        <?php>echo I made this page using nginx, mariadb, php-fpm and phpadmin. Everything was automated using Docker Compose and GitHub Actions. For further insight into the project, please check out my GitHub repository: <a href="https://github.com/ilknurm/lempstack">https://github.com/ilknurm/lempstack</a></p>'; ?>
+        <?php>echo '<p>I made this page using nginx, mariadb, php-fpm and phpadmin. Everything was automated using Docker Compose and GitHub Actions. For further insight into the project, please check out my GitHub repository: <a href="https://github.com/ilknurm/lempstack">https://github.com/ilknurm/lempstack</a></p>'; ?>
     </body>
 </html>
