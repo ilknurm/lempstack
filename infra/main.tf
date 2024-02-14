6d0968714769b8d7b86b6dba039ad9af9baea31c
ilknurm <ilknurmanav06@gmail.com>
2024-02-14 11:58:44 +0000

    add files

---
+++ infra/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "project" {
+  ami                     = "ami-058bd2d568351da34"
+  instance_type           = "t2.medium"
+  key_name                = "redhat-key"
+  vpc_security_group_ids = [ aws_security_group.php-sg.id ]
+  tags = {
+    Name = "project"
+  }
+  user_data = <<-EOF
+              #! /bin/bash
+              # Add Docker's official GPG key:
+              sudo apt-get update
+              sudo apt-get install ca-certificates curl
+              sudo install -m 0755 -d /etc/apt/keyrings
+              sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+              sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+              # Add the repository to Apt sources:
+              echo \
+              "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+              $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+              sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+              sudo apt-get update
+              sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+              sudo apt-get install docker-compose-plugin
+              EOF
+}
+  
+
+
+
