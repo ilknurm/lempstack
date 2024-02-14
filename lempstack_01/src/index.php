45a83935d8fdc27113f4b44b3ac92efb422cb093
ilknurm <ilknur.manav2@gmail.com>
2024-02-14 14:43:18 +0100

    update php

--- lempstack_01/src/index.php
+++ lempstack_01/src/index.php
@@ -7,7 +7,8 @@
         <?php echo '<p>Hello CHECK24</p>'; ?>
         <?php echo '<p>I made this page using nginx, mariadb, 
         php-fpm and phpadmin. Everything was automated using 
-        Docker Compose and GitHub Actions. For further insight into the project, 
+        Docker Compose and GitHub Actions.This is currently being hosted on an EC2 instance in AWS which was brought up using Terraform.
+         For further insight into the project, 
         please check out my GitHub repository: <a href="https://github.com/ilknurm/lempstack">https://github.com/ilknurm/lempstack</a></p>'; ?>
     </body>
     </body>
